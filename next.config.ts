ffc6b9d6e1b7c4abe9fa238b6124b7cbbc62bfac
dopassss <zakaria.akhfas@gmail.com>
2025-01-21 22:37:24 +0700

    Change output configuration to 'standalone' in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
+  output: 'standalone',
   eslint: {
     ignoreDuringBuilds: true,
   },
