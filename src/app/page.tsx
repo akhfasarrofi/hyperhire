6ef011d4aa744f1744cf5ff5650e7cd816491377
dopassss <zakaria.akhfas@gmail.com>
2025-01-21 22:58:50 +0700

    Update fetch request in page.tsx to disable caching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { LandingPage } from '@/components/templates/LandingPage';
 export const dynamic = 'force-dynamic'
 
 export default async function Page() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/talents`);
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/talents`, {cache: 'no-store'});
   const talents = await res.json();
   
   return <LandingPage talents={talents} />;
