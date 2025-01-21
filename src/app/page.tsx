2c8502feeb772c243364b520d30ba1e0005b212f
dopassss <zakaria.akhfas@gmail.com>
2025-01-21 23:00:06 +0700

    Update fetch request in page.tsx to use 'force-cache' for caching strategy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { LandingPage } from '@/components/templates/LandingPage';
 export const dynamic = 'force-dynamic'
 
 export default async function Page() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/talents`, {cache: 'no-store'});
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/talents`, {cache: 'force-cache'});
   const talents = await res.json();
   
   return <LandingPage talents={talents} />;
