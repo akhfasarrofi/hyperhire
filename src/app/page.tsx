4459fe8aadf0a448207ff756f9698b9fbd978dc4
dopassss <Zakaria_Akhfas@outlook.co.id>
2025-01-21 22:24:10 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { LandingPage } from '@/components/templates/LandingPage';
 
+export const dynamic = 'force-dynamic'
+
 export default async function Page() {
   const res = await fetch('http://localhost:3000/api/talents');
   const talents = await res.json();
